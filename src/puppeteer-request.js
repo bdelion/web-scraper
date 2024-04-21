e5988e4bdb0d226d5a08b2137d21570d34837c8d
Bertrand DELION <bertrand.delion@free.fr>
2024-04-21 16:07:59 +0200

    Add stabler.tech scripts

---
+++ src/puppeteer-request.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+async function scrapeDynamicContent(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, {waitUntil: 'networkidle2'});
+
+    const data = await page.evaluate(() => {
+        return document.querySelector('h1').innerText;
+    });
+
+    console.log(data);
+    await browser.close();
+}
+
+scrapeDynamicContent('https://www.meteociel.fr/temps-reel/obs_villes.php?affint=1&code2=79049004&jour2=20&mois2=1&annee2=2022');
